89e96c005833aafb3b6989a7588a2fa31a91f303
James Cartledge <jcartledge@gmail.com>
2011-01-11 22:38:20 +1100

    better launcher

--- vim-aliases.sh
+++ vim-aliases.sh
@@ -10,40 +10,77 @@ type -P mvim &>/dev/null && GUI_VIM=mvim
 type -P gvim &>/dev/null && GUI_VIM=gvim
 if [ ! $GUI_VIM ]; then echo "No GUI Vim available, can't install aliases."&>2; exit 1; fi
 
-# launch files in vim
-# support regular commands e.g. +num, +/search/
 function launch_vim {
-  
-  # if ! [ -t 0 ]; then stdin=`cat`; fi
-  if [ $# -gt 0 ]; then
-    if ! [ -t 0 ]; then
+
+  existing_vim=0; if [ -n `$GUI_VIM --serverlist` ]; then existing_vim=1; fi
+  pipe_supplied=0; if [ ! -t 0 ]; then pipe_supplied=1; fi
+  args_supplied=0; if [ $# -ne 0 ]; then args_supplied=1; fi
+
+  if [ $existing_vim -ne 1 ] && [ $args_supplied -ne 1 ] && [ $pipe_supplied -ne 1 ]; then
+    echo "0"
+    start_vim
+    bring_vim_to_front
+  elif [ $existing_vim -ne 1 ] && [ $args_supplied -ne 1 ] && [ $pipe_supplied -eq 1 ]; then
     echo "1"
-      echo "$stdin" | $GUI_VIM -f --remote-silent $* - 2>/tmp/vim-error.log&
-    else
+    start_vim
+    bring_vim_to_front
+    pipe_stdin_to_vim
+  elif [ $existing_vim -ne 1 ] && [ $args_supplied -eq 1 ] && [ $pipe_supplied -ne 1 ]; then
     echo "2"
-      $GUI_VIM -f --remote-silent $* 2>/tmp/vim-error.log&
-    fi
-  elif [ ! `$GUI_VIM --serverlist` ]; then
-    if [ -n "$stdin" ]; then
+    call_vim_with_args $*
+    bring_vim_to_front
+  elif [ $existing_vim -ne 1 ] && [ $args_supplied -eq 1 ] && [ $pipe_supplied -eq 1 ]; then
     echo "3"
-      echo "$stdin" | $GUI_VIM -f - 2>/tmp/vim-error.log&
-    else
+    call_vim_with_args $*
+    bring_vim_to_front
+    pipe_stdin_to_vim
+  elif [ $existing_vim -eq 1 ] && [ $args_supplied -ne 1 ] && [ $pipe_supplied -ne 1 ]; then
     echo "4"
-      $GUI_VIM -f 2>/tmp/vim-error.log&
-    fi
-  elif ! [ -t 0 ]; then
-    # existing instance, no additional args, input on STDIN
+    bring_vim_to_front
+  elif [ $existing_vim -eq 1 ] && [ $args_supplied -ne 1 ] && [ $pipe_supplied -eq 1 ]; then
     echo "5"
-    type -P wmctrl &>/dev/null && wmctrl -xa gvim.Gvim
-    $GUI_VIM --remote-send "<esc>:ene<cr>"
-    while read stdin
-    do
-      $GUI_VIM --remote-send "<esc>Gi$stdin<cr><esc>"
-    done
+    bring_vim_to_front
+    new_buffer_vim
+    pipe_stdin_to_vim
+  elif [ $existing_vim -eq 1 ] && [ $args_supplied -eq 1 ] && [ $pipe_supplied -ne 1 ]; then
+    echo "6"
+    bring_vim_to_front
+    call_vim_with_args $*
+  elif [ $existing_vim -eq 1 ] && [ $args_supplied -eq 1 ] && [ $pipe_supplied -eq 1 ]; then
+    echo "7"
+    bring_vim_to_front
+    call_vim_with_args $*
+    new_buffer_vim
+    pipe_stdin_to_vim
   fi
-  # TODO: gvim specific, mac-ify also
-  # bring to front
+}
+
+function start_vim {
+  echo "starting $GUI_VIM"
+  $GUI_VIM -f 2>/tmp/vim-error.log&
+}
+
+function call_vim_with_args {
+  echo "calling $GUI_VIM with args: $*"
+  $GUI_VIM -f --remote-silent $* 2>/tmp/vim-error.log&
+}
+
+function bring_vim_to_front {
+  echo "bringing $GUI_VIM to front"
   type -P wmctrl &>/dev/null && wmctrl -xa gvim.Gvim
+}
+
+function new_buffer_vim {
+  "opening new buffer in $GUI_VIM"
+  $GUI_VIM --remote-send "<esc>:ene<cr>"
+}
+
+function pipe_stdin_to_vim {
+  echo "piping STDIN to $GUI_VIM"
+  while read stdin
+  do
+    $GUI_VIM --remote-send "<esc>Gi$stdin<cr><esc>"
+  done
 }
 
 # open tag definition in vim
