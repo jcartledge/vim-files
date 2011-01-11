d91e08ea22c942dffbc5486b6ffb69ceb0ed2872
James Cartledge <jcartledge@gmail.com>
2011-01-11 11:58:08 +1100

    basic support for piping stdin

--- vim-aliases.sh
+++ vim-aliases.sh
@@ -2,21 +2,32 @@
 #
 # open in an existing instance
 # bring vim to the front
+# handle STDIN from pipe
 # launch vim with a tty
 
 # first make sure we have a vim
 type -P mvim &>/dev/null && GUI_VIM=mvim
 type -P gvim &>/dev/null && GUI_VIM=gvim
-if [ ! $GUI_VIM ]; then echo "No GUI Vim available, can't install aliases."&>2; exit 1; fi;
+if [ ! $GUI_VIM ]; then echo "No GUI Vim available, can't install aliases."&>2; exit 1; fi
 
 # launch files in vim
 # support regular commands e.g. +num, +/search/
 function launch_vim {
+  
+  if ! [ -t 0 ]; then stdin=`cat`; fi
   if [ $# -gt 0 ]; then
+    # TODO: handle STDIN here - not sure what that means
     $GUI_VIM -f --remote-silent $* 2>/tmp/vim-error.log&
   elif [ ! `$GUI_VIM --serverlist` ]; then
-    $GUI_VIM -f 2>/tmp/vim-error.log&
+    if [ -n "$stdin" ]; then
+      # STDIN, no existing vim
+      echo "$stdin" | $GUI_VIM -f - 2>/tmp/vim-error.log&
+    else
+      # no STDIN, no existing vim
+      $GUI_VIM -f 2>/tmp/vim-error.log&
+    fi
   fi
+  # TODO: handle STDIN here too.
   type -P wmctrl &>/dev/null && wmctrl -xa gvim.Gvim
 }
 
@@ -32,3 +43,10 @@ function launch_vim_tag {
 
 alias v=launch_vim
 alias t=launch_vim_tag
+
+
+# $ ls | gvim vim-aliases.sh -
+# > VIM - Vi IMproved 7.2 (2008 Aug 9, compiled Sep 28 2010 07:11:04)
+# > Too many edit arguments: "-"
+# > More info with: "vim -h"
+
